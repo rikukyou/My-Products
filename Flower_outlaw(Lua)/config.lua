1a47f75b30cd390db115d659e04c2379622c5b24
riku kyou <amerie_lj@hotmail.com>
2014-07-19 15:14:57 +0900

    Save the Flower(game)

---
+++ Flower_outlaw(Lua)/config.lua
@@ -0,0 +1,23 @@
+
+-- cpmgen config.lua
+application =
+{
+	content =
+	{
+		width = 320,
+		height = 480,
+		scale = "letterbox",
+		fps = 30,
+		antialias = false,
+		xalign = "center",
+		yalign = "center",
+		graphicsCompatibility = 1,
+
+		imageSuffix = 
+		{
+			["@2"] = 2
+			
+		}
+
+	}
+}
